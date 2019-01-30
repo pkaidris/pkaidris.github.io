17911dae1e318f538e02880e142c20ae2a060447
Kazeem Adebola Idris <34292756+pkaidris@users.noreply.github.com>
2019-01-30 13:53:33 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Kazeem Adebola Idris."],
       typeSpeed: 100,
       loop: true,
     });
